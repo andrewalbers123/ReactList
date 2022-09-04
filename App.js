51134afa080509f12425270b7c6857aee86ebbe3
Andrew <andrewalbers@gmail.com>
2022-09-04 11:38:10 -0700

    added components

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Keyboard, StyleSheet, Text, View, ScrollView } from 'react-native';
+import { Swipeable } from react-native-gesture-handler; 
+import TaskInputField from './components/TaskInputField';
+import TaskItem from './components/TaskItem';
 
 export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (task) => {
+    if (task == null) return;
+    setTasks([...tasks, task]);
+    Keyboard.dismiss();
+  }
+
+  const deleteTask = (deleteIndex) => {
+    setTasks(tasks.filter((value, index) => index != deleteIndex));
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>To-Do List</Text>
+      <ScrollView style={styles.scrollView}>
+        {
+          tasks.map((task, index) => {
+          return (
+            <View key={index} style={styles.taskContainer}>
+              <TaskItem index={index + 1} task={task} deleteTask={() => deleteTask(index)}/>
+            </View>
+          );
+          })
+        }
+      </ScrollView>
+      <TaskInputField addTask={addTask}/>
     </View>
   );
 }
@@ -13,8 +40,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#1E1A3C',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  heading: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: '600',
+    marginTop: 30,
+    marginBottom: 10,
+    marginLeft: 20,
+  },
+  taskContainer: {
+    marginTop: 20,
+  }
 });
